8d1307445d0c7c255783af2c5aabc7206546f107
Felix Pacis <felix.pacis@cognitedata.com>
2024-12-06 16:25:10 +0100

    feat: add result summarizer

--- Agent_ila/main.py
+++ Agent_ila/main.py
@@ -6,6 +6,7 @@ from agent_dev import IlaFilter
 from src.queryStream import query_ila
 from src.logs_filters import append_to_json
 from src.query_preprocess import query_preprocess
+from src.summarizer import summarizer
 load_dotenv()
 
 def main(user_query: str, client):
@@ -14,7 +15,7 @@ def main(user_query: str, client):
     ila_filter = IlaFilter()
     generated_ila_filter = ila_filter.generate_ila_filter( client, user_query)
 
-
+    
     #save in a .txt file
     # with open('generated_f_prompt.txt', 'w') as f:
     #     f.write(ila_filter.filter_prompt)
@@ -29,7 +30,7 @@ def main(user_query: str, client):
     return ila_result 
 
 if __name__ == "__main__":
-    raw_user_query = "In wells 15-F-11 A and 15-F-1 C, which formations were drilled?"
+    raw_user_query ="In wells 15-F-11 A and 15-F-1 C, which formations were drilled?" #"Which drill bits were used for 8.5 and 17.5 sizes?"
     #"What is the average inclination that the Hugin formation is drilled?"
     #"which formations are drilled in well 15-F-1 C"# and 15/9-F-11 A
 
@@ -44,6 +45,9 @@ if __name__ == "__main__":
     except Exception as e:
         print("Error during Cognite client initialization or fetching data:", e)
 
+
+    
+
     #"What is the average ROP in the Hidra formation?" 
     # #"find all the the wells with hugin formation"#What is the average ROP in the Hidra formation?"
     #"find all the the wells with hugin formation"#"find all the formations that are drilled between depths of 50 and 100 meters"
@@ -57,3 +61,6 @@ if __name__ == "__main__":
         print(single_user_query)
         result = main(single_user_query, client)  # Call the function
         print(result)  # Print the return value
+        print(summarizer(client).single_summary(single_user_query, result))
+
+
