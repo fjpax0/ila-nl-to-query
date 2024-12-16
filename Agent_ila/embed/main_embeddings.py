006481e7c2840daa33a76038abb77e8f9fc87f81
Felix Pacis <felix.pacis@cognitedata.com>
2024-12-16 12:41:06 +0100

    fix: save the metata as json with  not as dict

--- Agent_ila/embed/main_embeddings.py
+++ Agent_ila/embed/main_embeddings.py
@@ -24,9 +24,9 @@ def _format_jsonl_entry(jsonl_entry):
     data = jsonl_entry
 
     # Extract constants, user_query, and generated_query
-    constants = data.get("constants", {})
+    constants = json.dumps(data.get("constants", {}))
     user_query = data.get("user_query", "")
-    generated_query = data.get("generated_query", {})
+    generated_query = json.dumps(data.get("generated_query", {}))
 
     # Format the output
     formatted = (
@@ -47,6 +47,7 @@ def main():
         for line in file:
             # Parse the JSON content from the current line
             record = json.loads(line)
+   
             
             # Access fields from the JSON object
             user_query = record.get("user_query", "No user query found")
@@ -62,10 +63,10 @@ def main():
             assert len(embeddings) == 1, "Expected one embedding to be returned because we iterate over the jsonl file"
     
             vectors_list = [{'id':embeddings[0]['text'], "values": embeddings[0]['embedding'], 'metadata': {'ila_sample':_format_jsonl_entry(record)}}]
-            print(vectors_list)
+   
             # Upsert embeddings to Pinecone
             pc.insert(vector=vectors_list, namespace='user_query')
-            print(f"Inserted embeddings for user query: {user_query}")
+
 
             # Upsert embeddings to Pinecone
 
